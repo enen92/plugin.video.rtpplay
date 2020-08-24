338401834c82353e88d4657b2072dded62b4bd9e
Philipp Schmidt <Schmidt.Philipp94@gmail.com>
2020-08-24 18:01:09 +0200

    Fix channel URLs

--- resources/lib/channels.py
+++ resources/lib/channels.py
@@ -9,56 +9,49 @@ RTP_CHANNELS = [
     {   "id": "rtp1",
         "name" : "RTP 1",
         "streams": [
-            {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtp1HD.smil/playlist.m3u8"},
-            {"type":"dashwv", "url": "https://streaming-live.rtp.pt/liverepeater/rtp1.smil/manifest.mpd", "tk": "https://www.rtp.pt/play/direto/rtp1", "license": "https://widevine-proxy.drm.technology/proxy"}
+            {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtp1HD.smil/playlist.m3u8?pxt_rtp"}
         ]
     },
     {   "id": "rtp2",
         "name" : "RTP 2",
         "streams": [
-            {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtp2.smil/playlist.m3u8"},
+            {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtp2.smil/playlist.m3u8?pxt_rtp"}
         ]
     },
     {   "id": "rtp3",
         "name" : "RTP 3",
         "streams": [
-             {"type":"hls", "url": "https://streaming-live.rtp.pt/livetvhlsDVR/rtpndvr.smil/playlist.m3u8?DVR"},
-             {"type":"dashwv", "url": "https://streaming-live.rtp.pt/liverepeater/rtpn.smil/manifest.mpd?DVR", "tk": "https://www.rtp.pt/play/direto/rtp3", "license": "https://widevine-proxy.drm.technology/proxy" }
+             {"type":"hls", "url": "https://streaming-live.rtp.pt/livetvhlsDVR/rtpndvr.smil/playlist.m3u8?DVR"}
         ]
     },
     {   "id": "rtpinternacional",
         "name" : "RTP Internacional",
         "streams": [
-             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpi.smil/playlist.m3u8"},
-             {"type":"dashwv", "url": "https://streaming-live.rtp.pt/liverepeater/rtpi.smil/manifest.mpd", "tk": "https://www.rtp.pt/play/direto/rtpinternacional", "license": "https://widevine-proxy.drm.technology/proxy" }
+             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpi.smil/playlist.m3u8?pxt_rtp"}
         ]
     },
     {   "id": "rtpmemoria",
         "name" : "RTP Memória",
         "streams": [
-             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpmem.smil/playlist.m3u8"},
-             {"type":"dashwv", "url": "https://streaming-live.rtp.pt/liverepeater/rtpmem.smil/manifest.mpd", "tk": "https://www.rtp.pt/play/direto/rtpmemoria", "license": "https://widevine-proxy.drm.technology/proxy" }
+             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpmem.smil/playlist.m3u8?pxt_rtp"}
         ]
     },
     {   "id": "rtpmadeira",
         "name" : "RTP Madeira",
         "streams": [
-             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpmadeira.smil/playlist.m3u8"},
-             {"type":"dashwv", "url": "https://streaming-live.rtp.pt/liverepeater/rtpmadeira.smil/manifest.mpd", "tk": "https://www.rtp.pt/play/direto/rtpmadeira", "license": "https://widevine-proxy.drm.technology/proxy" }
+             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpmadeira.smil/playlist.m3u8?pxt_rtp"}
         ]
     },
     {   "id": "rtpacores",
         "name" : "RTP Açores",
         "streams": [
-             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpacores.smil/playlist.m3u8"},
-             {"type":"dashwv", "url": "https://streaming-live.rtp.pt/liverepeater/rtpacores.smil/manifest.mpd", "tk": "https://www.rtp.pt/play/direto/rtpacores", "license": "https://widevine-proxy.drm.technology/proxy" }
+             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpacores.smil/playlist.m3u8?pxt_rtp"}
         ]
     },
     {   "id": "rtpafrica",
         "name" : "RTP África",
         "streams": [
-             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpafrica.smil/playlist.m3u8"},
-             {"type":"dashwv", "url": "https://streaming-live.rtp.pt/liverepeater/rtpafrica.smil/manifest.mpd", "tk": "https://www.rtp.pt/play/direto/rtpafrica", "license": "https://widevine-proxy.drm.technology/proxy" }
+             {"type":"hls", "url": "https://streaming-live.rtp.pt/liverepeater/smil:rtpafrica.smil/playlist.m3u8?pxt_rtp"}
         ]
     },
 ]
